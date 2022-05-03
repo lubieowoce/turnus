0f90fb221ee8d7808a07113f6709c504ebbf14f2
Janka Uryga <lolzatu2@gmail.com>
2022-05-03 12:39:57 +0200

    use react-location

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { useState } from 'react';
 import {
+  Flex,
+  Heading,
   ThemeProvider,
 } from 'theme-ui';
 import { MapView } from './map'
 import { theme } from './theme';
+import {
+  Link,
+  Outlet,
+  ReactLocation,
+  Route,
+  Router,
+} from '@tanstack/react-location'
 
 
 const Root = () => {
@@ -18,14 +27,69 @@ const Root = () => {
       }
     })
   );
+  const [location] = useState(() => new ReactLocation());
+
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
-        <App />
+        <Router
+          location={location}
+          routes={routes}
+        />
+        {/* <App /> */}
       </ThemeProvider>
     </QueryClientProvider >
   );
 }
+
+
+const MainLayout = () => {
+  const linksLeft = [
+    <Link to={'/miejscowosci'}>Miejscowości</Link>,
+    <Link to={'/mapa'}>Mapa</Link>,
+    <Link to={'/wystawy'}>Wystawy</Link>,
+  ]
+  const linksRight = [
+    <Link to={'/info'}>Info</Link>
+  ]
+  return (
+    <div>
+      <Flex>
+        {linksLeft.map((el) => <div style={{ padding: '1em'}}>{el}</div>)}
+        <div style={{ flex: '1 auto' }} />
+        {linksRight.map((el) => <div style={{ padding: '1em'}}>{el}</div>)}
+      </Flex>
+      <Outlet />
+    </div>
+  )
+}
+
+const routes: Route[] = [
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      {
+        path: 'miejscowosci',
+        element: <Heading>Miejscowości</Heading>
+      },
+      {
+        path: 'mapa',
+        element:  <Heading>Mapa</Heading>
+      },
+      {
+        path: 'wystawy',
+        element: <Heading>Wystawy</Heading>
+      },
+      {
+        path: 'info',
+        element: <Heading>Info</Heading>
+      },
+    ],
+  }
+]
+
+
 
 export default Root;
 
