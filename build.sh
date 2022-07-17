d13edca99af282ae0a8303035af86380695a4b12
Janka Uryga <lolzatu2@gmail.com>
2022-07-17 12:50:38 +0200

    remove old junk from build scripts

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-DIR="$PWD"
-
 # pushd frontend
 # yarn run build
 # popd
@@ -11,10 +9,6 @@ export DOCKER_BUILDKIT=1
 
 pushd frontend-server
 ./build.sh --tag 'skadinad/frontend-server'
-# # old version without tar magic:
-# [ -d frontend-server/assets ] && rm -rf frontend-server/assets
-# cp -R frontend/build frontend-server/assets/
-# docker build . -t skadinad/frontend-server --build-arg ASSETS_SOURCE_DIR="./assets"
 popd
 
 pushd backend-server
--- push-images.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -eu -o pipefail;
-
-IMAGE_NAMES='skadinad/frontend-server skadinad/backend-server'
-printf $'%s\n' $IMAGE_NAMES | xargs -n1 -P4 ./sync-image.sh
