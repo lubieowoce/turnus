34633859a9040db063d422cad0d0eb8f3936f494
Janka Uryga <lolzatu2@gmail.com>
2022-07-17 11:33:41 +0200

    Dockerfile insanity: enable compiling JS assets before the docker build and passing the result in Building everything on a remote docker host is cool and all, but my VPS doesn't have enough memory to run a full webpack build :(

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,10 @@
-FROM node:18-alpine AS frontend_builder
+ARG PREBUILT_JS=1
+
+# strap in, it's time to do control flow in a Dockerfile!
+# see https://stackoverflow.com/a/60820156
+
+# if PREBUILT_JS=0 :
+FROM node:18-alpine AS frontend_builder_0
 
 WORKDIR /working
 COPY frontend/package.json frontend/yarn.lock ./
@@ -8,6 +14,15 @@ COPY frontend/src ./src
 COPY frontend/public ./public
 COPY frontend/tsconfig.json ./
 RUN yarn run build
+
+# if PREBUILT_JS=1 :
+FROM scratch as frontend_builder_1
+WORKDIR /working
+COPY frontend/build ./build
+
+# do the branch (via interpolation) and make the result available under a single name,
+# because we can't interpolate in a COPY --from="..."
+FROM "frontend_builder_${PREBUILT_JS}" as frontend_builder
 
 
 FROM node:18-alpine
--- build.sh
+++ build.sh
@@ -1,16 +1,36 @@
+#!/usr/bin/env bash
+set -x
+PREBUILT_JS="${PREBUILT_JS:-1}"
+export DOCKER_BUILDKIT=1
+
 function make-context-tarball() {
   # a little trickery to include frontend assets into the tarball
   # (remapping the path to './assets' using -s, because Docker wants everything in the same directory)
+  FRONTEND_EXCLUDES=$(
+    printf -- "--exclude ../frontend/node_modules/" ;
+    if [ $PREBUILT_JS = 0 ] ; then
+      printf -- "--exclude ../frontend/build/" ;
+    fi
+  )
+  FRONTEND_INCLUDES=$(
+    if [ $PREBUILT_JS = 0 ] ; then
+      printf "../frontend" ;
+    else
+      printf "../frontend/build" ;
+    fi
+  )
   tar -c \
     --exclude-vcs --exclude-from '.dockerignore' \
-    --exclude '../frontend/build/' --exclude '../frontend/node_modules/' \
+    $FRONTEND_EXCLUDES \
     -s '|../frontend/|./frontend/|' \
-    '../frontend/' '.'
+    $FRONTEND_INCLUDES \
+    '.'
 }
-export DOCKER_BUILDKIT=1
+
+# ctx=$(mktemp) && make-context-tarball > $ctx && tar -t -f $ctx && exit 0;
 
 make-context-tarball | docker build \
   "$@" \
-  -f ./Dockerfile --build-arg ASSETS_SOURCE_DIR="./assets" \
+  -f ./Dockerfile --build-arg "PREBUILT_JS=$PREBUILT_JS" \
   --build-arg BUILDKIT_INLINE_CACHE=1 \
   -
