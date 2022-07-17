be39eb288e6050b685b6e7cc4613ae7c3c57d1a9
Janka Uryga <lolzatu2@gmail.com>
2022-07-18 00:42:13 +0200

    fix build paths & remove dockerfile insanity

--- build.sh
+++ build.sh
@@ -3,18 +3,20 @@ set -eu -o pipefail
 
 BUILD_JS="${BUILD_JS:-0}"
 if [ $BUILD_JS = 1 ]; then
-  pushd frontend
+  pushd frontend/app
   yarn run build
   popd
-  export PREBUILT_JS=1
+  pushd frontend/server
+  yarn run build
+  popd
 else
   echo 'skipping JS build (enable with BUILD_JS=1)'
 fi
 
 export DOCKER_BUILDKIT=1
 
-pushd frontend-server
-./build.sh --tag 'skadinad/frontend-server'
+pushd frontend
+./build.sh --tag 'skadinad/frontend-server' --no-cache
 popd
 
 pushd backend-server
--- frontend/build.sh
+++ frontend/build.sh
@@ -1,35 +1,16 @@
 #!/usr/bin/env bash
 set -x
-PREBUILT_JS="${PREBUILT_JS:-1}"
 export DOCKER_BUILDKIT=1
 
 function make-context-tarball() {
-  # a little trickery to include frontend assets into the tarball
-  # (remapping the path to './assets' using -s, because Docker wants everything in the same directory)
-  FRONTEND_EXCLUDES=$(
-    printf -- "--exclude ../frontend/node_modules/" ;
-    if [ $PREBUILT_JS = 0 ] ; then
-      printf -- "--exclude ../frontend/build/" ;
-    fi
-  )
-  FRONTEND_INCLUDES=$(
-    if [ $PREBUILT_JS = 0 ] ; then
-      printf "../frontend" ;
-    else
-      printf "../frontend/build" ;
-    fi
-  )
   tar -c \
-    --exclude-vcs --exclude-from '.dockerignore' \
-    $FRONTEND_EXCLUDES \
-    -s '|../frontend/|./frontend/|' \
-    $FRONTEND_INCLUDES \
-    './Dockerfile' './build' './package.json' './yarn.lock'
+    './app/build' \
+    './Dockerfile' './server/build' './server/package.json' './server/yarn.lock'
 }
 
 
 make-context-tarball | docker build \
   "$@" \
-  -f ./Dockerfile --build-arg "PREBUILT_JS=$PREBUILT_JS" \
+  -f ./Dockerfile \
   --build-arg BUILDKIT_INLINE_CACHE=1 \
   -
