ab73376da3312407ad08865618ba5cc48f15ead8
Janka Uryga <lolzatu2@gmail.com>
2022-07-18 22:24:47 +0200

    fix docker build after workspaces setup but now Docker caching is busted because of https://github.com/yarnpkg/yarn/issues/5703

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-pushd frontend
-yarn run build
-popd
-
 export DOCKER_BUILDKIT=1
 
 pushd frontend
--- frontend/build.sh
+++ frontend/build.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
+set -eu -o pipefail
 set -x
 export DOCKER_BUILDKIT=1
 
 function make-context-tarball() {
   tar -c \
-    './app/build' \
-    './Dockerfile' './server/build' './server/package.json' './server/yarn.lock'
+    './Dockerfile' \
+    './yarn.lock' './package.json' \
+    './app/build' `#'./app/package.json'` \
+    './server/build' './server/package.json'
 }
 
+npx turbo run build
 
 make-context-tarball | docker build \
   "$@" \
