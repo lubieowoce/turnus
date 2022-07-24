61f9d84cb39668afe720642b42180eb2644a2bde
Janka Uryga <lolzatu2@gmail.com>
2022-07-24 17:28:58 +0200

    responsive layout in place overview

--- frontend/app/src/place-details.tsx
+++ frontend/app/src/place-details.tsx
@@ -33,7 +33,7 @@ export const PlaceDetails = memo(({
         {!imageSets.length && (
           <Text sx={{ fontStyle: 'italic' }}>{'Nie ma tu jeszcze żadnych prac.'}</Text>
         )}
-        <Flex sx={{ gap: '2em', flexDirection: 'column', alignItems: 'stretch' }}>
+        <Flex sx={{ gap: ['4em', '2em'], flexDirection: 'column', alignItems: 'stretch' }}>
           {imageSets.map(([id, imageSetSummary]) =>
             <ImageSetSummary key={id} imageSet={imageSetSummary} />
           )}
@@ -43,31 +43,65 @@ export const PlaceDetails = memo(({
   );
 })
 
+const makeImageDims = (heightEm) => {
+  return {
+    height: `${heightEm}em`,
+    width: `${(4/3) * heightEm}em`,
+  }
+}
+
+
+const _imageDims = [makeImageDims(10), makeImageDims(15), makeImageDims(20)]
+const imageDims = {
+  width: _imageDims.map((d) => d.width),
+  height: _imageDims.map((d) => d.height),
+}
+
 const ImageSetSummary = ({ imageSet }: { imageSet: ImageSetType }) => {
-  const imageHeightEm = 20;
-  const imageWidthEm = (4/3) * imageHeightEm;
-  const imageHeight = `${imageHeightEm}em`;
-  const imageWidth = `${imageWidthEm}em`;
   return (
-    <Flex sx={{ width: '100%', maxWidth: '100%', height: imageHeight }}>
-      <Link variant='reset' to={`./${imageSet.id}`} sx={{ display: 'block', flex: '0 0 40ch' }}>
+    <Flex sx={{
+      flexDirection: ['column-reverse', null, 'row'],
+      width: '100%',
+      maxWidth: '100%',
+      transition: 'all ease-in-out 0.1s',
+      background: 'rgba(0,0,0, 0)',
+      outline: '1em solid rgba(0,0,0, 0)',
+      '&:hover': {
+        backgroundColor: 'rgba(0,0,0, 0.05)',
+        outlineColor: 'rgba(0,0,0, 0.05)',
+      }
+    }}>
+      <Link
+        variant='reset' to={`./${imageSet.id}`}
+        sx={{
+          display: 'block',
+          flex: [null, null, '0 0 40ch'],
+          marginRight: [PADDING_BODY.horizontal, null, 'unset'],
+        }}
+      >
         <Box>
           <Heading as='h2'>{imageSet.title}</Heading>
           <Heading as='h3'>{imageSet.author}</Heading>
           <br />
-          <Paragraph sx={{ fontSize: 0 }}>{imageSet.summary}</Paragraph>
+          <Paragraph sx={{ fontSize: 0 }}>{imageSet.summary} ⟶</Paragraph>
         </Box>
       </Link>
       <Spacer />
-      <Flex sx={{ overflowY: 'clip', overflowX: 'auto', gap: '1em', flex: '1 auto' }}>
+      <Flex sx={{
+        overflowY: 'clip',
+        overflowX: 'auto',
+        gap: '1em',
+        flex: '1 auto',
+        height: imageDims.height,
+      }}>
         {Object.values(imageSet.media).map(({ url }) =>
           <Link variant='reset' key={url} to={`./${imageSet.id}`} sx={{ flex: 'none' }}>
             <Image
               src={url}
               sx={{
                 objectFit: 'cover',
-                width: imageWidth,
-                height: imageHeight,
+                width: imageDims.width,
+                height: imageDims.height,
                 backgroundColor: 'lightgray'
               }}
             />
