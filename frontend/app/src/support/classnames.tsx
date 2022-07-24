d95e0454681604994d1218fcababa9dd9464e5a2
Janka Uryga <lolzatu2@gmail.com>
2022-07-24 22:14:45 +0200

    expose original css function in themed ClassNames wrapper

--- frontend/app/src/support/classnames.tsx
+++ frontend/app/src/support/classnames.tsx
@@ -1,19 +1,20 @@
-import { ClassNames as BaseClassNames } from "@emotion/react";
+import { ClassNames as BaseClassNames, ClassNamesContent } from "@emotion/react";
 import { ReactNode } from "react";
 import { ThemeUIStyleObject } from "theme-ui";
 import { useApplyTheme } from "./use-apply-theme";
 
 type GetClassNamesFn = (sx: ThemeUIStyleObject) => string;
+type Consumer = (cls: GetClassNamesFn, original: ClassNamesContent) => ReactNode
 
 type ClassNamesProps =
-  | { children: (cls: GetClassNamesFn) => ReactNode }
-  | { render: (cls: GetClassNamesFn) => ReactNode }
+  | { children: Consumer }
+  | { render: Consumer }
 
 export const ClassNames = (props: ClassNamesProps) => {
   const applyTheme = useApplyTheme();
-  const render = ({ css }) => {
-    const cls: GetClassNamesFn = (sx) => css(applyTheme(sx));
-    return ('render' in props ? props.render : props.children)(cls);
+  const render = (original: ClassNamesContent) => {
+    const cls: GetClassNamesFn = (sx) => original.css(applyTheme(sx));
+    return ('render' in props ? props.render : props.children)(cls, original);
   };
   return (
     <BaseClassNames>{render}</BaseClassNames>
