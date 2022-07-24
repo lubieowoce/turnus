6abdcdd1125c625bc9768023efd5e4f82e425bf3
Janka Uryga <lolzatu2@gmail.com>
2022-07-24 22:14:30 +0200

    replace lightgallery with new lightbox

--- frontend/app/src/image-set.tsx
+++ frontend/app/src/image-set.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import { memo, useMemo, useState } from 'react';
 import {
   Box,
   Heading,
@@ -9,15 +9,45 @@ import {
   Container,
 } from 'theme-ui';
 
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css'
+import { disableBodyScroll, enableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock/lib/bodyScrollLock.esm';
+
 import {
+  ImageMediaObject,
   ImageSetDetails as ImageSetDetailsType,
 } from "./api";
+
 
 export const ImageSetDetails = memo(({
   imageSet,
 }: {
   imageSet: ImageSetDetailsType,
 }) => {
+  const images = useMemo(() => Object.values(imageSet.media), [imageSet.media]);
+  const [lightbox, setLightbox] = useState({ imageIndex: 0, isOpen: false });
+
+  // the lightbox doesn't expose a wrapper ref or anything,
+  // so we need to do it the old way
+  const lightboxClass = `lightbox-wrapper-${imageSet.id}`;
+
+  const lockScroll = () => {
+    const el = document.getElementsByClassName(lightboxClass).item(0);
+    if (!el) {
+      return;
+    }
+    disableBodyScroll(el);
+  }
+  const unlockScroll = () => {
+    const el = document.getElementsByClassName(lightboxClass).item(0);
+    if (!el) {
+      // something weird happened, make sure we're not stuck with a locked scroll
+      clearAllBodyScrollLocks()
+      return;
+    }
+    enableBodyScroll(el);
+  }
+
   return (
     <Box>
       <Heading as='h1' sx={{ flex: '1 1 auto' }}>
@@ -31,13 +61,38 @@ export const ImageSetDetails = memo(({
           sx={{ fontFamily: 'body' }}
           dangerouslySetInnerHTML={{ __html: imageSet.content }}
         />
-        <ImageSetGallery imageSet={imageSet} />
+        <ImageSetGallery
+          images={images}
+          onImageClick={(imageIndex) => setLightbox({ isOpen: true, imageIndex })}
+        />
+        {lightbox.isOpen && (
+          <Lightbox
+            wrapperClassName={lightboxClass}
+            mainSrc={images[lightbox.imageIndex].url}
+            nextSrc={images[plusMod(lightbox.imageIndex, +1, images.length)].url}
+            prevSrc={images[plusMod(lightbox.imageIndex, -1, images.length)].url}
+            reactModalProps={{ shouldFocusAfterRender: false }}
+            onAfterOpen={lockScroll}
+            onCloseRequest={() => { setLightbox({ isOpen: false, imageIndex: 0 }); unlockScroll(); }}
+            onMoveNextRequest={() => setLightbox((p) => ({ ...p, imageIndex: plusMod(p.imageIndex, +1, images.length)}))}
+            onMovePrevRequest={() => setLightbox((p) => ({ ...p, imageIndex: plusMod(p.imageIndex, -1, images.length)}))}
+          />
+        )}
       </Box>
     </Box>
   );
-})
+});
 
-const ImageSetGallery = ({ imageSet }: { imageSet: ImageSetDetailsType }) => {
+// (+) with wraparound, for positive and negative values
+const plusMod = (n: number, delta: number, mod: number) =>
+  ((n % mod) + (delta % mod) + mod) % mod
+
+type ImageSetGalleryProps = {
+  images: ImageMediaObject[],
+  onImageClick?: (index: number) => void,
+}
+
+const ImageSetGallery = ({ images, onImageClick }: ImageSetGalleryProps) => {
   return (
     <Box sx={{
       display: 'grid',
@@ -45,11 +100,17 @@ const ImageSetGallery = ({ imageSet }: { imageSet: ImageSetDetailsType }) => {
       gap: '10px 10px',
       pb: '3',
     }}>
-      {Object.values(imageSet.media).map(({ url }) =>
+      {images.map(({ url }, index) =>
         <AspectRatio ratio={1 / 1} key={url}>
           <Image
             src={url}
-            sx={{ objectFit: 'cover', height: '100%', width: '100%' }}
+            sx={{
+              objectFit: 'cover',
+              height: '100%',
+              width: '100%',
+              cursor: onImageClick ? 'pointer' : 'unset',
+            }}
+            onClick={onImageClick && (() => onImageClick(index))}
           />
         </AspectRatio>
       )}
