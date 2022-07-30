8d044af5d4ae6716518a6ab79a6cb08446f7948a
Janka Uryga <lolzatu2@gmail.com>
2022-07-30 14:40:33 +0200

    backend: initial events-section implementation

--- backend-server/update-files.sh
+++ backend-server/update-files.sh
@@ -4,8 +4,10 @@ set -eu -o pipefail;
 TEMPLATE_DIR="$1"
 TARGET_DIR="$2"
 
+# copy over our site template
+
 cd $TEMPLATE_DIR
-for FILE in $(find -type f \! -name .gitkeep \! -name .gitignore -printf '%P\n'); do
+for FILE in $(find -type f \! -name .gitignore -printf '%P\n'); do
   DEST="$TARGET_DIR/$FILE"
   mkdir -p $(dirname "$DEST")
   echo "copying $FILE --> $DEST"
@@ -14,3 +16,24 @@ done
 
 chown -R xyz "$TARGET_DIR"
 chgrp -R xyz "$TARGET_DIR"
+
+# we just copied our stuff to the user/ dir.
+# unfortunately, having stuff in user/ will make the docker-grav init-admin script
+# skip copying the user/ dir from the base grav install.
+# which means we won't get any plugins, so no admin interface!
+# (it'd be cleaner to just change init-admin altogether, because we're unzipping twice,
+#  but this easier for now)
+
+# this is adapted from the main docker-grav init-admin script,
+# but changed to only extract & copy plugins)
+
+mkdir -p $TARGET_DIR
+if [[ -f '/grav/grav.zip' ]]; then
+  WORKING_DIR=$(mktemp -d);
+  echo "copying plugins from /grav/grav.zip... (working directory: $WORKING_DIR)";
+  unzip -q '/grav/grav.zip' 'grav-admin/user/plugins/*' -d "$WORKING_DIR"
+  cp -r "$WORKING_DIR"/grav-admin/user/plugins/* "$TARGET_DIR/plugins/"
+  echo "copied plugins:"
+  ls "$TARGET_DIR/plugins/"
+  rm -rf "$WORKING_DIR"
+fi
---
+++ dev-pages/events/skadinad-wernisaz-1/event.md
@@ -0,0 +1,10 @@
+---
+title: 'Cośtam cośtam'
+media_order: 'img1.png,img2.png,img3.png'
+location: Warszawa
+time: '01.07.2022'
+---
+
+🏠 Skądinąd to pierwsze z cyklu wydarzeń, których pomysłodawcami są Turnusowicze poznani dzięki grupie *wszyscy na turnus wszyscy to turnus*. Podczas poniedziałkowych grupowych spotkań online rozmawialiśmy o wielu rzeczach, poznawaliśmy się i wymyślaliśmy wspólne działania. Pierwszym z nich jest wystawa **Skądinąd**, na której zaprezentujemy prace wyłonione podczas zimowego open calla.
+
+Gdzieś trzeba zacząć.
