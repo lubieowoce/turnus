a5330116843b489984cb4d185915c1e61adf126a
Janka Uryga <lolzatu2@gmail.com>
2022-08-05 14:50:22 +0200

    new horizontal layout for place details

--- frontend/app/src/place-details.tsx
+++ frontend/app/src/place-details.tsx
@@ -3,17 +3,16 @@ import {
   Box,
   Heading,
   Paragraph,
-  Flex,
   Close,
   Image,
   Text,
+  Flex,
 } from 'theme-ui';
 
-import { ImageSet as ImageSetType, PlaceDetails as PlaceDetailsType } from "./api";
+import { ImageMediaObject, ImageSet as ImageSetType, PlaceDetails as PlaceDetailsType } from "./api";
 import { useGoBack } from './utils';
 import { PADDING_BODY } from './config';
 import { Link } from './support/themed-link';
-import { EntryWithImages } from './entry-with-images';
 
 export const PlaceDetails = memo(({
   place,
@@ -23,36 +22,51 @@ export const PlaceDetails = memo(({
   const close = useGoBack();
   const imageSets = Object.entries(place.imageSets);
   return (
-    <Box sx={{ marginBottom: '2em' }}>
-      <Flex py='1em' px={PADDING_BODY.horizontal} mb='3em'>
-        <Heading as='h1' sx={{ flex: '1 1 auto' }}>
+    <Flex sx={{ flexDirection: 'column', alignItems: 'stretch', height: '100%', paddingBottom: ['2em', 'unset'] }}>
+      <Flex py='1em' px={PADDING_BODY.horizontal} mb='1em' sx={{ flex: 'none'}}>
+        <Heading as='h2' sx={{ flex: '1 1 auto' }}>
           {place.name}
         </Heading>
         <CloseButton onClick={close} />
       </Flex>
-      <Box sx={{ paddingLeft: PADDING_BODY.horizontal }}>
+      <Box sx={{ flex: '1 auto'}}>
         {!imageSets.length && (
           <Text sx={{ fontStyle: 'italic' }}>{'Nie ma tu jeszcze żadnych prac.'}</Text>
         )}
-        <Flex sx={{ gap: ['4em', '2em'], flexDirection: 'column', alignItems: 'stretch' }}>
-          {imageSets.map(([id, imageSetSummary]) =>
-            <EntryWithImages
-              link={`./${imageSetSummary.id}`}
-              description={
-                <Box>
-                  <Heading as='h2'>{imageSetSummary.title}</Heading>
-                  <Heading as='h3'>{imageSetSummary.author}</Heading>
-                  <br />
-                  <Paragraph sx={{ fontSize: 0 }}>{imageSetSummary.summary} ⟶</Paragraph>
-                </Box>
-              }
-              images={Object.values(imageSetSummary.media)}
-              key={id}
-            />
-          )}
+        <Flex
+          sx={{
+            minHeight: '100%',
+            gap: ['4em', '2em'],
+            flexDirection: ['column', 'row'],
+            alignItems: 'stretch',
+            overflowX: [undefined, 'auto'],
+            overflowY: [undefined, 'hidden'],
+            paddingLeft: PADDING_BODY.horizontal,
+            paddingRight: PADDING_BODY.horizontal,
+          }}>
+          {imageSets.map(([id, imageSetSummary]) => {
+            const link = `./${imageSetSummary.id}`;
+            return (
+              <VerticalEntry
+                link={link}
+                image={Object.values(imageSetSummary.media)[0]}
+                description={
+                  <Link variant='reset' to={link}>
+                    <Box mt='1em'>
+                      <Heading as='h2'>{imageSetSummary.title}</Heading>
+                      <Heading as='h3'>{imageSetSummary.author}</Heading>
+                      <br />
+                      <Paragraph sx={{ fontSize: 0, maxHeight: `${4 * 1.5}em`, overflow: 'hidden', textOverflow: 'ellipsis' }}>{imageSetSummary.summary} ⟶</Paragraph>
+                    </Box>
+                  </Link>
+                }
+                key={id}
+              />
+            )
+          })}
         </Flex>
       </Box>
-    </Box>
+    </Flex>
   );
 })
 
@@ -61,3 +75,26 @@ export const PlaceDetails = memo(({
 const CloseButton = ({ onClick }) => (
   <Close onClick={onClick} sx={{ cursor: 'pointer' }} />
 )
+
+
+const imageWidths = ['100%', `20em`, `25em`];
+
+const VerticalEntry = ({ image, link, description }: { image: ImageMediaObject, link: string, description: React.ReactNode }) => {
+  return (
+    <Box sx={{ width: imageWidths, flex: 'none' }}>
+      <Link variant='reset' key={image.url} to={link}>
+        <Image
+          src={image.url}
+          sx={{
+            objectFit: 'cover',
+            width: imageWidths,
+            // height: imageDims.height,
+            backgroundColor: 'lightgray',
+            aspectRatio: '4 / 3',
+          }}
+        />
+      </Link>
+      {description}
+    </Box>
+  )
+}
